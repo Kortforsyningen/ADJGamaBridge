ab55eee5e37e42bf6a11192659715d9c803ee1e8
Kristian Evers <kristianevers@gmail.com>
2017-11-03 13:36:49 +0100

    Added test calculation

---
+++ examples/vestkyst.bat
@@ -0,0 +1,5 @@
+python ..\gamaAdjKMS.py -s ..\data\koordinater -s ..\data\koter -o ..\data\r_obs_fix -o ..\data\r_vand -f "G.M.182" vestkyst\setup.xml
+
+gama-local.exe vestkyst\setup.xml --xml vestkyst\out.xml --html vestkyst\out.html
+
+
