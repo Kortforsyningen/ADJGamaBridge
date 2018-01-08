5aed7e384826112cf578476d508698f3297e2a61
Simon Krarup Steensen <sikrs@sdfe.dk>
2018-01-08 14:25:28 +0100

    Tekst er overskueligt opstillet i scriptet

--- output_to_pubdvr90.py
+++ output_to_pubdvr90.py
@@ -5,7 +5,8 @@ Created on Fri Nov 17 11:16:55 2017
 @author: b035777 - Simon Krarup Steensen
 
 """
-#import time                                                                    Tidstagning brugt i starten til at teste fart af forskellige metoder
+#Tidstagning brugt i starten til at teste fart af forskellige metoder
+#import time                                                                    
 #startTime = time.time()    
 
 import datetime
@@ -15,8 +16,10 @@ from math import sqrt
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("path_input", help="Path of output file from Gnu-Gama", type=str)                   #Defining name and path of the input xml file
-parser.add_argument('pub_out', help='Full path(and name) of the text pub file', type=str)               #Defining name and path of the pub text file
+#Defining name and path of the input xml file
+parser.add_argument("path_input", help="Path of output file from Gnu-Gama", type=str)                   
+#Defining name and path of the pub text file
+parser.add_argument('pub_out', help='Full path(and name) of the text pub file', type=str)               
 
 args = parser.parse_args()
 
@@ -37,8 +40,10 @@ tree = ET.parse(file_path_out)
 root = tree.getroot()
 
 index = 0
-for child in root:                                                              #Finder nødvendige index til sub_roots, upåvirket ved nye elementer i out.xml filen
-    if child.tag.endswith('}coordinates'):                                      #Bruger endswith, og medtager }, hvis nu et andet tag som slutter på det samme introducres
+#Finder nødvendige index til sub_roots, upåvirket ved nye elementer i out.xml filen
+for child in root:                                                              
+#Bruger endswith, og medtager }, hvis nu et andet tag som slutter på det samme introducres
+    if child.tag.endswith('}coordinates'):                                      
         coor_sub_root_ind = index
         coor_sub_root = child
     if child.tag.endswith('}network-processing-summary'):
@@ -53,9 +58,11 @@ for child in root:                                                              
 index = 0
 
 description = root[description_index].text
-description_dict = dict(e.split(':') for e in description.split(','))           #Description tagget som er passeret gennem GnuGama laves til en dictionary
+#Description tagget som er passeret gennem GnuGama laves til en dictionary
+description_dict = dict(e.split(':') for e in description.split(','))           
 
-for child in network_para_sub_root:                                             #De 3 loops kommer frem det totale z-count, altså antal koder til indlæsning
+#De 3 loops kommer frem det totale z-count, altså antal koder til indlæsning
+for child in network_para_sub_root:                                             
     if child.tag.endswith('}coordinates-summary'):
         for child in child:
             if child.tag.endswith('}coordinates-summary-adjusted'):
@@ -67,8 +74,9 @@ for child in network_para_sub_root:                                             
     index = index + 1
 index=0
 
-for child in coor_sub_root:                                                     #Loop som finder index for de forskellige subroots der skal bruges
-    if child.tag.endswith('}adjusted'):                                         #Adjusted 
+#Loop som finder index for de forskellige subroots der skal bruges
+for child in coor_sub_root:                                                     
+    if child.tag.endswith('}adjusted'):                                         
         adj_subsub_root_ind = index
 #        print child.tag
     if child.tag.endswith('}fixed'):
@@ -77,7 +85,8 @@ for child in coor_sub_root:                                                     
     if child.tag.endswith('}cov-mat'):
         cov_subsub_root_ind = index
 #        print child.tag
-    if child.tag.endswith('}original-index'):                                   #Bruges pt. ikke. Det er ikke index til cov-mat!
+    if child.tag.endswith('}original-index'):                                   
+        #Bruges pt. ikke. Det er ikke index til cov-mat!
         ori_subsub_root_ind = index
 #        print child.tag
     index = index + 1
@@ -85,25 +94,32 @@ for child in coor_sub_root:                                                     
 original_index = []
 for line in coor_sub_root[ori_subsub_root_ind]:                                 
     original_index.append(int(line.text))
+
+#Gemmer alle elementer i cov-mat,
 cov = []
-for line in coor_sub_root[cov_subsub_root_ind]:                                 #Gemmer alle elementer i cov-mat,
-    if line.tag.endswith('}flt') is False:                                      #Bortset fra linjer der ikke slutter på flt. Siden den indeholder stadard linjer om størrelse
-        continue                                                                #Næste step i loop
+for line in coor_sub_root[cov_subsub_root_ind]:                                 
+    #Bortset fra linjer der ikke slutter på flt. Siden den indeholder stadard linjer om størrelse
+    if line.tag.endswith('}flt') is False:                                      
+        continue                                                            
+        #Næste step i loop
     cov.append(float(line.text))
                 
 pub_name = args.pub_out 
 #f = open('pubdvr_xml2text', 'w')
 f = open(pub_name, 'w')
-f.write('#DK_wor   DK_h_dvr90\n')                                               #3 af de 4 linjer er standard
+f.write('#DK_wor   DK_h_dvr90\n')                                               
         
-file_time_unix = os.path.getmtime(file_path_out)                                #Tager tidspunkt for tidspunkt på out filen
-gnu_gama_time = datetime.datetime.fromtimestamp(int(file_time_unix)).strftime('%Y%m%d,%H.%M')   #Unix til ønsket format
+#Tager tidspunkt for tidspunkt på out filen
+file_time_unix = os.path.getmtime(file_path_out)                                
+#Unix tid ønsket format
+gnu_gama_time = datetime.datetime.fromtimestamp(int(file_time_unix)).strftime('%Y%m%d,%H.%M')   
 
 f.write('beregningsdato    ' + str(gnu_gama_time) + '\n')
 f.write('prodnr       4081\nbnorm           0\n')
 f.write('\n\n')
 
-for line in coor_sub_root[fixed_subsub_root_ind]:                               #Nedskriver x-antal fix punkter. Øverste fix-punkts linjer
+#Nedskriver x-antal fix punkter. Øverste fix-punkts linjer
+for line in coor_sub_root[fixed_subsub_root_ind]:                               
     year = description_dict[line[0].text+'f']
     f.write('{:>17s}'.format( line[0].text))
     f.write('{:>14.5f}m  {}    {}\n'.format(round( float(line[1].text), 5), year ,
@@ -112,19 +128,26 @@ for line in coor_sub_root[fixed_subsub_root_ind]:                               
 f.write('\n\n-1x\n')
 
 i=0
-for line in root[coor_sub_root_ind][adj_subsub_root_ind]:                       #Looper indskriver alle datapunkterne
-    year = description_dict[line[0].text]                                       #Tager dict fra description til at finde årstallet
-    if line[0].text[0:2].endswith('-'):                                         #Data punkter med kun et bogstav f.eks K, bliver K + mellemrum og resten
+#Loopet indskriver alle datapunkterne
+for line in root[coor_sub_root_ind][adj_subsub_root_ind]:                       
+    #Tager dict fra description til at finde årstallet
+    year = description_dict[line[0].text]                                       
+    
+    if line[0].text[0:2].endswith('-'):                                         
+        #Data punkter med kun et bogstav f.eks K, bliver K + mellemrum og resten
         line[0].text=line[0].text[0] + ' ' + line[0].text[1:]                               
     f.write('{:>17s}{:>14.5f}m'.format( line[0].text , float(line[1].text)))
     f.write('{:^8} {:08} {:02} {}'.format(year , 00000000 , 2, 1))
-#    sigma=sqrt(cov[original_index.index(i+1)])                                 #Første antagelse om cov-mat rækkefølge. Original index bruges som rækkefølge, FORKERT!!!
-    sigma = sqrt(cov[i])                                                        #Antager samme rækkefølge i cov-matn som data, hvilket er den rigtige metode
+                                
+    #Antager samme rækkefølge i cov-matn som data, hvilket er den rigtige metode
+    sigma = sqrt(cov[i])                                                        
     sigma_app = int(round( sigma , 0 ))
     f.write(' {:03}\n'.format(sigma_app))
-    i=i+1                                                                       #i bruges som index i listen med original index
+    #i bruges som index i listen med original index
+    i=i+1                                                                       
 
 f.write('-1z')
 f.close()
 
-#print ('The script took {} seconds!'.format(time.time() - startTime))          #printer tiden for script eksekvering
+ #printer tiden for script eksekvering
+#print ('The script took {} seconds!'.format(time.time() - startTime))         
